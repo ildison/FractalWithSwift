691091c42a1c9dfc9f116ea4061023c29535ad7d
ildison <ildarusmanov92@gmail.com>
2020-06-02 17:01:54 +0300

    add UIImageview

---
+++ Fractal/Fractal/Fractal.swift
@@ -0,0 +1,18 @@
+//
+//  Fractal.swift
+//  Fractal
+//
+//  Created by Ildar Usmanov on 02.06.2020.
+//  Copyright © 2020 Ildar Usmanov. All rights reserved.
+//
+
+import Foundation
+
+struct Complex {
+    var re: Double
+    var im: Double
+}
+
+class Fractal {
+    
+}
---
+++ Fractal/Fractal/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Fractal
+//
+//  Created by Ildar Usmanov on 02.06.2020.
+//  Copyright © 2020 Ildar Usmanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
